0a0f2f69af5fc0f0c6cd49546a745b7c976c2969
Dan Liverance <daniel.liverance@gmail.com>
2021-03-13 16:45:11 -0500

    Dan controls Dan and Marco controls Marco

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System;
+using System.Text;
+
+public class PlayerController : MonoBehaviour
+{
+    // Marco's IP: 184.147.95.146
+    // Dan's IP: 64.137.136.12
+
+    public float Speed = 1.0f;
+
+    private Socket mSocket = null;
+
+
+    public readonly IPEndPoint mDanEndPoint = new IPEndPoint(new IPAddress(new byte[] { 64, 137, 136, 12 }), 35353);
+    public readonly IPEndPoint mMarcoEndPoint = new IPEndPoint(new IPAddress(new byte[] { 184, 147, 95, 146 }), 35353);
+
+    private IPEndPoint mChosenEndPoint = null;
+
+    void Start()
+    {
+        // Marco controls the Marco GameObject... Dan controls the Dan GameObject
+        if (!gameObject.name.Contains(Environment.UserName))
+        {
+            enabled = false;
+            return;
+        }
+
+        if (Environment.UserName == "Dan")
+        {
+            mChosenEndPoint = mMarcoEndPoint;
+        }
+        else
+        {
+            mChosenEndPoint = mDanEndPoint;
+        }
+
+        mSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    }
+
+    void Update()
+    {
+        float deltaTime = Time.deltaTime;
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(horizontal * Speed * deltaTime, vertical * Speed * deltaTime, 0.0f);
+
+        ClampPositionToScreen();
+
+        string message = $"{transform.position.x}|{transform.position.y}";
+
+        mSocket.SendTo(Encoding.UTF8.GetBytes(message), mChosenEndPoint);
+    }
+
+
+    void ClampPositionToScreen()
+    {
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+
+        float x = screenPosition.x;
+        float y = screenPosition.y;;
+
+        x = Mathf.Clamp(x, 0, Camera.main.pixelWidth);
+        y = Mathf.Clamp(y, 0, Camera.main.pixelHeight);
+
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(x, y, screenPosition.z));
+    }
+}
